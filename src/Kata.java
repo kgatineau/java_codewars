764eb5bb857eb7dc3cb210b672a4a6bb01accd2f
Kaitlyn Gatineau <gati0010@algonquinlive.com>
2022-05-13 11:03:53 -0600

    Codewars - May 13th 2022 - Java

--- src/Kata.java
+++
@@ -1,9 +0,0 @@
-public class Kata {
-    public static boolean solution(String str, String ending) {
-        return  str.endsWith(ending);
-    }
-
-    public static void main(String[] args) {
-        solution("abc", "bc");
-    }
-}
