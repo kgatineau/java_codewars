e5459d95c9c306585ab8ba13cd966a1b0797b3a6
Kaitlyn Gatineau <gati0010@algonquinlive.com>
2022-05-23 23:17:04 -0600

    Codewars - May 23rd 2022 - Java

---
+++ src/CountingDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author Kaitlyn Gatineau
+ * Codewars Java - May 23rd
+ */
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+
+        return Arrays.stream(
+                Arrays.stream(text.toLowerCase().split("")).toArray())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(m -> m.getValue() > 1).map(Map.Entry::getKey).toList().size();
+    }
+
+    public static void main(String[] args) {
+        duplicateCount("abcde");
+    }
+}
