6597be66861a55f26fe3f7c1b01c4a3d68b2a39d
Kaitlyn Gatineau <gati0010@algonquinlive.com>
2022-06-23 11:42:25 -0600

    'Playing with cubes I' - June 23 2022

---
+++ src/Cube.java
@@ -0,0 +1,11 @@
+public class Cube{
+    // An easy kata I started and did not finish while at 8 kyu
+    private int side = 0;
+    public int getSide(){
+    return side;
+    }
+    public void setSide(int side){
+    this.side = side;
+    }
+
+}
